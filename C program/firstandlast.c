9eb291ed8f7e250196452cb234f170b0b2ded223
Ashi-Ashirbaditya <129829025+Ashi-Ashirbaditya@users.noreply.github.com>
2025-11-20 17:58:54 +0530

    Update firstandlast.c

--- C program/firstandlast.c
+++ C program/firstandlast.c
@@ -4,6 +4,7 @@ void main()
     int rem,n,fst;
     printf("Enter a number:");
     scanf("%d",&n);
+    lst = n % 10;
     while(n>0)
     {
         rem=n%10;
@@ -11,5 +12,6 @@ void main()
         n=n/10;
     }
     printf("1st number =%d\n",fst);
-    printf("%d",n);
+    printf("%d",lst);
+
 }
